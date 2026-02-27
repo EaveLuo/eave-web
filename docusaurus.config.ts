bf19c92181727164554c725735730fef80a005e6
EaveLuo <846899026@qq.com>
2026-02-27 09:00:17 +0800

    docs: 添加 AI 栏目 intro 页面

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -182,7 +182,7 @@ const config: Config = {
             },
             {
               label: 'AI',
-              to: '/docs/ai/',
+              to: '/docs/ai/intro',
             },
           ],
         },
